696cf9a065fe944e1600d6e1bb9216d861a2eb9e
davidjohn66 <davidjohnsutherland66@gmail.com>
2024-07-28 12:43:40 -0400

    test3

--- dragon.py
+++ dragon.py
@@ -1,1 +1,17 @@
-print('hello')
+class Exp:
+    def __init__(self, base, exponent):
+        self.base = base
+        self.exponent = exponent
+
+class Reader:
+    def __init__(self, text):
+        self.text = text
+
+code = '''
+
+x = 1
+
+'''
+
+r = Reader(code)
+print(r.text)
